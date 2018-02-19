d27ef0cf134ba3abb6bba8b0c2a142cc52a5d305
myskam7 <mksmflppv98@gmail.com>
2018-02-19 10:10:55 -0500

    ajax req

--- mernapp/src/CommentBox.js
+++ mernapp/src/CommentBox.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 import style from './style';
@@ -10,13 +11,29 @@ class CommentBox extends Component {
         this.state = {
             data : []
         };
+        this.loadCommentFromServer = this.loadCommentFromServer.bind(this);
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+    loadCommentFromServer() {
+        axios.get(this.props.url)
+        .then(res => {
+            this.setState({ data: res.data });
+        })
+    }
+    handleCommentSubmit(comment) {
+        //Add post req
+    }
+      
+    componentDidMount() {
+        this.loadCommentFromServer();
+        setInterval(this.loadCommentFromServer, this.props.pollInterval);
     }
     render() {
         return (
             <div style={ style.CommentBox }>
             <h2>Comments</h2>
             <CommentList data={ this.state.data }/>
-            <CommentForm />
+            <CommentForm onCommentSubmit={ this.handleCommentSubmit} />
             </div>
         )
     }
