1acfcd8c0b9961cad6d7c145414116c5cd2dc0fb
myskam7 <mksmflppv98@gmail.com>
2018-02-19 11:55:34 -0500

    put delete

--- mernapp/server.js
+++ mernapp/server.js
@@ -59,6 +59,30 @@ router.route('/comment')
     });
 });
 
+router.route('/comment/:comment_id')
+
+.put((req, res) => {
+    Comment.findById(req.params.comment_id, (err, comment) => {
+        if(err) 
+        res.send(err); 
+        (req.body.author) ? comment.author = req.body.author : null;
+        (req.body.text) ? comment.text = req.body.text : null;
+
+        comment.save(err => {
+            if (err) 
+            res.send(err);
+            res.json({ message: 'Comment has been updated' });
+        });
+    });
+})
+.delete((req, res) => {
+    Comment.remove({_id: req.params.comment_id }, (err, comment) => {
+        if(err)
+        res.send(err);
+        res.json({message: "Comment has been deleted"})
+    })
+});
+
 
 
 
